09f53f8cdbf7aab1a565666f4801e7ceb6f91620
qJFI <ymkhhk2005@gmail.com>
2024-05-17 20:25:42 +0300

    Project Base

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,36 @@ namespace Multi_Media_Minecraft_Project_YM_MT
     public partial class Form1 : Form
     {
         Bitmap off;
+        Bitmap BackImg = new Bitmap("Images/Back.jpg");
         public Form1()
         {
-            
+
+            this.WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            Paint += Form1_Paint;
+              
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDouble(e.Graphics);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            off = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+        void DrawScene(Graphics g)
+        {
+            g.Clear(Color.White);
+            g.DrawImage(BackImg, 0, 0,ClientSize.Width,ClientSize.Height);
+        }
+        void DrawDouble(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
         }
     }
 }
